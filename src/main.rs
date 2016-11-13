b52387008cfd4adf657e47127f1aa3e62bd8a4af
Alex Bowen <ajbowen249@gmail.com>
2016-11-12 19:06:34 -0500

    basic spin and handlng message log events

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ extern crate rustc_serialize;
 use hyper::client::Client;
 use std::io::Read;
 use rustc_serialize::json;
+use std::{thread, time};
 
 static EVENT_SERVER_ADDR: &'static str = "http://pickleandmuffin.com/eventServer/api.php";
+const POLL_THROTTLE_MS: u64 = 100;
 
 fn main() {
     println!("Starting up home client.");
@@ -22,16 +24,83 @@ fn main() {
         }
     }
 
-    
+    let mut sequence_number: i64 = -1;
+
+    match get_highest_sequence_number(){
+        Some(highest_num) => sequence_number = highest_num,
+        None =>{
+            println!("Failed to get initial sequence number :(");
+            return;
+        }
+    }
+
+    println!("Observing events after sequence number {}", sequence_number);
+
+    loop{
+        match get_chunk_of_events(sequence_number){
+            Some(vec) =>{
+                for item in &vec {
+                    process_event(item);
+                }
+
+                sequence_number = vec[vec.len() - 1].sequenceNumber;
+            }
+            _ => {}
+        }
+
+        let poll_throttle = time::Duration::from_millis(POLL_THROTTLE_MS);
+        thread::sleep(poll_throttle);
+    }
+}
+
+fn process_event(event: &Event) {
+    match event.eventType.as_ref() {
+        "Message" => process_message_event(event),
+        _ => {}
+    }
+}
+
+fn process_message_event(event: &Event) {
+    let message_event: MessageEventBody = json::decode(&event.body).unwrap();
+    println!("Got message event at {}: {}", event.time, message_event.message);
 }
 
 fn ping_event_server() -> bool {
-    let (status, _) = post_request(&EVENT_SERVER_ADDR.to_string(), &"{\"method\" : \"ping\"}".to_string());
+    let reqest_body = EventSeverRequest{ method: "ping".to_string(), parameters: "" };
+    let reqest_body = json::encode(&reqest_body).unwrap();
+
+    let (status, _) = post_request(&EVENT_SERVER_ADDR.to_string(), &reqest_body);
     return status == hyper::Ok;
 }
 
-fn get_max_sequence_number -> i64 {
+fn get_chunk_of_events(after_sequence_number: i64) -> Option<Vec<Event>>{
+    let reqest_body = EventSeverRequest{ method: "getChunkOfEvents".to_string(), parameters: GetChunkOfEventsRequest{ afterSequenceNumber: after_sequence_number }};
+    let reqest_body = json::encode(&reqest_body).unwrap();
 
+    let (status, response_body) = post_request(&EVENT_SERVER_ADDR.to_string(), &reqest_body);
+
+    match status {
+        hyper::Ok =>{
+            let result: EventServerResult<Vec<Event>> = json::decode(&response_body).unwrap();
+            return if result.result.len() > 0 { Some(result.result) } else { None };
+        }
+        _ => return None
+    }
+}
+
+fn get_highest_sequence_number() -> Option<i64> {
+    let reqest_body = EventSeverRequest{ method: "getHighestSequenceNumber".to_string(), parameters: "" };
+    let reqest_body = json::encode(&reqest_body).unwrap();
+
+    let (status, response_body) = post_request(&EVENT_SERVER_ADDR.to_string(), &reqest_body);
+
+    match status {
+        hyper::Ok =>{
+            let result: EventServerResult<SequenceNumberContainer> = json::decode(&response_body).unwrap();
+            return Some(result.result.sequenceNumber);
+        }
+        _ => return None
+    }
 }
 
 fn post_request(address: &String, body: &String) -> (hyper::status::StatusCode, String) {
@@ -45,4 +114,41 @@ fn post_request(address: &String, body: &String) -> (hyper::status::StatusCode, 
     let _ = result.read_to_string(&mut response_body);
 
     return (result.status, response_body);
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct EventSeverRequest<T> {
+    method: String,
+    parameters: T
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+#[allow(non_snake_case)]
+struct GetChunkOfEventsRequest {
+    afterSequenceNumber: i64
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct EventServerResult<T> {
+    result: T
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+#[allow(non_snake_case)]
+struct Event {
+    sequenceNumber: i64,
+    eventType: String,
+    time: String,
+    body: String
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+#[allow(non_snake_case)]
+struct SequenceNumberContainer {
+    sequenceNumber: i64
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct MessageEventBody {
+    message: String
 }
